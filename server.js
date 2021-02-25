2f14940b2592b5130dd6b526362243e5c6c11423
Kyle G <legokyle@frontier.com>
2021-02-24 19:40:44 -0800

    Added script to package.json file and added server.js file

---
+++ server.js
@@ -0,0 +1,21 @@
+//All of our required npm and files
+const express = require('express');
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+const db = require('./models');
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static('public'));
+
+// Syncing our sequelize models and then starting our Express app
+db.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+});
