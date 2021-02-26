5b36ed548e175de08be224b56237612feaca06de
Rebecca Howe <Rebeccahowe20@gmail.com>
2021-02-25 20:47:22 -0800

    create routes to html pages

---
+++ routes/html-routes.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = (app) => {
+
+    // Home Page
+    app.get('/', (req, res) =>
+        res.sendFile(path.join(__dirname, '../public/html/homepage.html'))
+    );
+
+    // Char Creation
+    app.get('/creation', (req, res) =>
+        res.sendFile(path.join(__dirname, '../public/html/char-creation.html'))
+    );
+
+    // Stats Page
+    app.get('/stats', (req, res) =>
+        res.sendFile(path.join(__dirname, '../public/html/stats.html'))
+    );
+
+    // Proficiencies page
+    app.get('/prof', (req, res) =>
+        res.sendFile(path.join(__dirname, '../public/html/prof.html'))
+    );
+
+    // Char Sheet page
+    app.get('/char-sheet', (req, res) =>
+        res.sendFile(path.join(__dirname, '../public/html/char-sheet.html'))
+    );
+
+    // Spell Selection
+    app.get('/spells', (req, res) =>
+        res.sendFile(path.join(__dirname, '../public/html/spells.html'))
+    );
+
+    // Spell List/Description
+    app.get('/spells-list', (req, res) =>
+        res.sendFile(path.join(__dirname, '../public/html/spells-list.html'))
+    );
+
+};
