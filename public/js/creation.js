43798de1eea158212a7ecc8d94b6436ac6d30f53
Mason M <m8mamiya@gmail.com>
2021-03-04 21:55:24 -0800

    add remainder of creation and stats local storage posts

--- public/js/creation.js
+++ public/js/creation.js
@@ -4,6 +4,9 @@ document.getElementById('submit-creation').addEventListener('click', (event) => 
         char_name: document.getElementById('characterName').value,
         player_name: document.getElementById('playerName').value,
         class: document.getElementById('classMenu').value,
+        level: document.getElementById('levelSelect').value,
+        race: document.getElementById('raceSelect').value,
+        background: document.getElementById('backgroundSelect').value,
     }
     window.localStorage.setItem('new-char', JSON.stringify(newChar))
     window.location.href='/stats'
